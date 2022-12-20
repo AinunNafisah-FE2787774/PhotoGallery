905f12857403855bb68f62079ea51fcc29ba5f0c
Ainun Nafisah <223200042@student.unaki.ac.id>
2022-12-20 09:44:58 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ainun Nafisah" // TODO: replace this
+const studentId = "FE2787774" // TODO: replace this
+
+export { url, studentName, studentId }
